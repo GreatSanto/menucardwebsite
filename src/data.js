b937e60e708a67576ae4cc4ca0e85ac1cd26b1b9
GreatSanto <834046+GreatSanto@users.noreply.github.com>
2021-08-20 00:30:53 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ const menu = [
     title: 'buttermilk pancakes',
     category: 'breakfast',
     price: 15.99,
-    img: './images/item-1.jpeg',
+    img: './images/item-2.jpeg',
     desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
   },
   {
@@ -12,7 +12,7 @@ const menu = [
     title: 'diner double',
     category: 'lunch',
     price: 13.99,
-    img: './images/item-2.jpeg',
+    img: 'images/item-2.jpeg',
     desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
   },
   {
